91ed204d56009035a5f58f6e885feef4dd591209
senya0ne <liketravelin@gmail.com>
2020-12-02 20:59:26 +0300

    add tests

---
+++ java/src/test/java/com/example/selenide/tests/java/MainPageTest.java
@@ -0,0 +1,17 @@
+package com.example.selenide.tests.java;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainPageTest {
+
+    @Test
+    public void openPage() {
+        open("https://selenide.org");
+        $("body").shouldHave(text("What is Selenide?"));
+
+    }
+}
